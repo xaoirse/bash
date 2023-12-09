b7b425c50299cee80ee1c5e62b2937fbd29881f4
xaoirse <mxsadegh@gmai.com>
2023-12-09 21:01:11 +0330

    bugs fixed

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-### simple fish argparse for bash
+# Simple fish argparse for bash
+
+### USAGE
+
+```bash
+source <(curl -s https://raw.githubusercontent.com/xaoirse/bash/main/functions.sh)
+
+# Examples
+    argparse "j:g:v" "rose -j jisoo -v lisa jennie -g blackpink"
+    
+    assert_eq "${opts[j]}" "jisoo"
+    assert_eq "${opts[g]}" "blackpink"
+    assert_eq "${opts[v]}" "true"
+    assert_eq "${opts[u]}" ""
+    assert_eq "${args[0]}" "rose"
+    assert_eq "${args[1]}" "lisa"
+    assert_eq "${args[2]}" "jennie"
+```
--- functions.sh
+++ functions.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
+# SA https://github.com/xaoirse/bash
+
 argparse() {
-    local OPTIND
-    local OPTARG
     unset opts
     unset args
     declare -gA opts
@@ -11,36 +11,35 @@ argparse() {
     params="$2"
 
     # set a: to value if -a <value>
-    for token in $(echo "$1" | grep -Eo '[[:alpha:]]{1}:'); do
+    for token in $(printf "%s" "$1" | grep -Eo '[[:alpha:]]{1}:'); do
         token=${token/:/}
-        value=$(echo "$2" | grep -oP "(^|\s)-[[:alpha:]]*?$token\s*\K(\S+)")
+        value=$(printf "%s" "$params" | grep -oP "(^|\s)-[[:alpha:]]*?$token\s*\K(\S+)")
         if [ -n "$value" ]; then
             opts["$token"]="$value"
             # shellcheck disable=SC2001
-            params="$(echo "$params" | sed -e "s/\(^\|\s\)-[[:alpha:]]*\?\($token\s*\($value\)\)/\1/")"
-            # params="$(echo "$params" | sed -e "/\(^\|\s\)-[[:alpha:]]*/s/$token//g")"
+            params="$(printf "%s" "$params" | sed -e "s/\(\(^\|\s\)\-[[:alpha:]]*\?\)\($token\s*$value\)/\1/")"
         else
-            echo "-$token should have a value"
+            printf "%s\n" "-$token must have a value"
             return 2
         fi
     done
 
-    # set a to set if -a
-    for token in $(echo "$1" | grep -Po '\K[[:alpha:]](?=[^:]|$)'); do
-        if echo "$2" | grep -Pq "(^|\s)-[[:alpha:]]*?$token"; then
+    # set a to true if -a
+    for token in $(printf "%s" "$1" | grep -Po '\K[[:alpha:]](?=[^:]|$)'); do
+        if printf "%s" "$params" | grep -Pq "(^|\s)-[[:alpha:]]*?$token"; then
             opts[$token]="true"
-            params="$(echo "$params" | sed -e "/\(^\|\s\)-[[:alpha:]]*/s/$token//g")"
+            params="$(printf "%s" "$params" | sed -e "/\(^\|\s\)-[[:alpha:]]*/s/$token//g")"
         fi
     done
 
     # return on unknown options
-    if echo "$params" | grep -Pq "(^|\s)-[[:alpha:]]+?"; then
-        echo "Unknown option"
+    if printf "%s" "$params" | grep -Pq "(^|\s)-[[:alpha:]]+?"; then
+        printf "%s\n" "Unknown option"
         return 3
     fi
 
     # shellcheck disable=SC2001
-    params="$(echo "$params" | sed 's,\(^\|\s\)-\+, ,g')"
+    params="$(printf "%s" "$params" | sed 's,\(^\|\s\)-\+, ,g')"
     # shellcheck disable=SC2206
     args=($params)
 }
@@ -51,30 +50,40 @@ test_argparse() {
     assert_eq "${opts[p]}" "jisoo"
     assert_eq "${opts[v]}" ""
     assert_eq "${args}" ""
+    unset opts
+    unset args
 
     argparse "p:v" "-vp jisoo"
     assert_eq "${opts[p]}" "jisoo"
     assert_eq "${opts[v]}" "true"
     assert_eq "${opts[u]}" ""
     assert_eq "${args}" ""
+    unset opts
+    unset args
 
     argparse "p:v" "-vp jisoo lisa"
     assert_eq "${opts[p]}" "jisoo"
     assert_eq "${opts[v]}" "true"
     assert_eq "${opts[u]}" ""
     assert_eq "${args}" "lisa"
+    unset opts
+    unset args
 
     argparse "p:v" "-p jisoo -v lisa"
     assert_eq "${opts[p]}" "jisoo"
     assert_eq "${opts[v]}" "true"
     assert_eq "${opts[u]}" ""
     assert_eq "${args}" "lisa"
+    unset opts
+    unset args
 
     argparse "p:v" "-p jisoo -v lisa"
     assert_eq "${opts[p]}" "jisoo"
     assert_eq "${opts[v]}" "true"
     assert_eq "${opts[u]}" ""
     assert_eq "${args}" "lisa"
+    unset opts
+    unset args
 
     argparse "p:v" "rose -p jisoo -v lisa jennie"
     assert_eq "${opts[p]}" "jisoo"
@@ -83,6 +92,8 @@ test_argparse() {
     assert_eq "${args[0]}" "rose"
     assert_eq "${args[1]}" "lisa"
     assert_eq "${args[2]}" "jennie"
+    unset opts
+    unset args
 
     argparse "p:n:v" "rose -p jisoo -v lisa jennie -n blackpink"
     assert_eq "${opts[p]}" "jisoo"
@@ -92,14 +103,33 @@ test_argparse() {
     assert_eq "${args[0]}" "rose"
     assert_eq "${args[1]}" "lisa"
     assert_eq "${args[2]}" "jennie"
+    unset opts
+    unset args
 
+    # Special case
+    argparse "p:n:v" "-vnp blackpink jisoo jennie"
+    assert_eq "${opts[p]}" "blackpink"
+    assert_eq "${opts[n]}" "jisoo"
+    assert_eq "${opts[v]}" "true"
+    assert_eq "${args[0]}" "jennie"
+    unset opts
+    unset args
+
+    assert_eq "$(argparse "p:" "-p")" "-p must have a value"
+    assert_eq "$(argparse "p:v" "-p")" "-p must have a value"
+    assert_eq "$(argparse "p:v" "-vp")" "-p must have a value"
+    assert_eq "$(argparse "v:p:" "-v blckpink -p")" "-p must have a value"
+
+    assert_eq "$(argparse "v" "-n")" "Unknown option"
+    assert_eq "$(argparse "v:p:" "-v blckpink -np bp")" "Unknown option"
 }
 
 assert_eq() {
-    if [ "$1" != "$2" ]; then
-        echo "right: $1"
-        echo "left : $2"
-        echo "Not equal"
-        return 2
-    fi
+    assert_eq() {
+        if [ "$1" != "$2" ]; then
+            printf "%s\n" "right: $1"
+            printf "%s\n" "left : $2"
+            return 2
+        fi
+    }
 }
